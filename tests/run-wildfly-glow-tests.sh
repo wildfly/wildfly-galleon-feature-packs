b3c4dd8547e5dc4037a602351070ada3cf3e81ff
Emmanuel Hugonnet <ehugonne@redhat.com>
2025-01-10 10:58:30 +0100

    Upgrade WildFly AI feature-pack to 0.3.2
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- tests/run-wildfly-glow-tests.sh
+++ tests/run-wildfly-glow-tests.sh
@@ -225,7 +225,7 @@ incubating
 
 echo Incubating ai feature-pack with opentelemetry support.
 test \
-"[ai, cdi, default-embedding-content-retriever, ee-integration, jaxrs, ollama-chat-model, opentelemetry, servlet]==>ee-core-profile-server,default-embedding-content-retriever,jaxrs,ollama-chat-model" \
+"[ai, cdi, default-embedding-content-retriever, ee-integration, jaxrs, ollama-chat-model, opentelemetry, servlet]==>ee-core-profile-server,default-embedding-content-retriever,jaxrs,ollama-chat-model,opentelemetry" \
 "tests/war/webchat-opentelemetry.war" \
 "" \
 "" \
